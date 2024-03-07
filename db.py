a4596112445bb2510b94365144a1908451edda57
Simon Wildsmith <s.wildsmith7@gmail.com>
2024-03-07 13:02:17 -0800

    Update function to improve performance

---
+++ db.py
@@ -0,0 +1,53 @@
+from sqlalchemy import create_engine, Column, Integer, Float, String, Date
+from sqlalchemy.orm import sessionmaker, declarative_base
+
+# Database Configuration
+DATABASE_URI = 'postgresql+psycopg2://postgres:postgres@localhost/db'
+
+engine = create_engine(DATABASE_URI)
+Session = sessionmaker(bind=engine)
+Base = declarative_base()
+
+class Equity(Base):
+    __tablename__ = 'equities'
+
+    id = Column(Integer, primary_key=True)
+    date = Column(Date)
+    ticker = Column(String)
+    price = Column(Float)
+    shares_outstanding = Column(Float)
+    pe_ratio = Column(Float)
+    gics_sector = Column(String)
+    gics_sub_industry = Column(String)
+
+class Commodity(Base):
+    __tablename__ = 'commodities'
+
+    id = Column(Integer, primary_key=True)
+    date = Column(Date)
+    GOLD = Column(Float)
+    COPPER = Column(Float)
+    CRUDE_OIL = Column(Float)
+    NICKEL = Column(Float)
+    PALLADIUM = Column(Float)
+    PLATINUM = Column(Float)
+    IRON = Column(Float)
+
+#series_id from FRED
+
+class MacroeconomicData(Base):
+    __tablename__ = 'macroeconomic_data'
+
+    id = Column(Integer, primary_key=True)
+    date = Column(Date)
+    unemployment_rate = Column(Float) #series_id: UNRATENSA
+    gdp_growth = Column(Float) #series_id: A191RP1Q027SBEA
+    real_gdp_growth = Column(Float) #series_id: A191RL1Q225SBEA
+    fed_funds_rate = Column(Float) #series_id: DFF
+    consumer_price_index = Column(Float) #series_id: CPIAUCSL
+    ten_year_minus_two_year_treasury = Column(Float) #series_id: T10Y2Y
+    m2_money_supply = Column(Float) #series_id: M2SL
+
+# Create tables
+if __name__ == '__main__':
+    Base.metadata.create_all(engine)
